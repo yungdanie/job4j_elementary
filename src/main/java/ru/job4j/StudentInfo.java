09e54359882d0eaa63d21b23b34a683fb31b99a5
yungdanie <nikoches253@gmail.com>
2022-02-16 19:47:34 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Daniil Kocheshkov");
+        System.out.println("12.1212.112001");
+    }
+}
