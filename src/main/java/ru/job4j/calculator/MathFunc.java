62a1fd3d290eb542dd46ed66454839530898b067
yungdanie <nikoches253@gmail.com>
2022-02-17 16:28:10 +0300

    4.2. Результат работы метода [#232622]

---
+++ src/main/java/ru/job4j/calculator/MathFunc.java
@@ -0,0 +1,17 @@
+package ru.job4j.calculator;
+
+public class MathFunc {
+        public static int func1(int x) {
+            int y = x * x + 1;
+            return y;
+        }
+
+        public static int func2(int x) {
+            int y = 1 / x;
+            return y;
+        }
+
+        public static void main(String[] args) {
+            int result3 = MathFunc.func1(100);
+        }
+}
