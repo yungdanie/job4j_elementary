935938f24b03db2cfdc98e5aebe79692fe9deb61
yungdanie <nikoches253@gmail.com>
2022-02-22 17:37:51 +0300

    5.1. Подсчет суммы чётных чисел в диапазоне [#192]

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -4,7 +4,18 @@ public class Counter {
     public static int sum(int start, int finish) {
         int sum = 0;
         for (int i = start; i <= finish; i++) {
-            sum = sum + start++;
+            sum = sum + i;
+        }
+        return sum;
+    }
+
+    public static int sumByEven(int start, int finish) {
+        int sum = 0;
+        if (start % 2 != 0) {
+            start = start + 1;
+        }
+        for (int i = start; i <= finish; i = i + 2) {
+            sum = sum + i;
         }
         return sum;
     }
@@ -13,5 +24,9 @@ public class Counter {
         System.out.println(sum(0, 10));
         System.out.println(sum(3, 8));
         System.out.println(sum(1, 1));
+
+        System.out.println(sumByEven(0, 10));
+        System.out.println(sumByEven(3, 8));
+        System.out.println(sumByEven(1, 1));
     }
 }
---
+++ src/test/java/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import org.junit.Assert;
+
+public class CounterTest {
+
+    @Test
+    public void whenSumEvenNumbersFromOneToTenThenThirty() {
+        int start = 1;
+        int finish = 10;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 30;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenSumEvenNumbersFromZeroToTenThenThirty() {
+        int start = 0;
+        int finish = 10;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 30;
+        Assert.assertEquals(expected, result);
+    }
+}
