7bc488a74ea5eb926201ca0d405bfb4c61fc9d1e
yungdanie <nikoches253@gmail.com>
2022-02-23 11:22:39 +0300

    5.0. Цикл for. Определение. [#3572 #268154]

--- src/main/java/ru/job4j/loop/LoopFor.java
+++ src/main/java/ru/job4j/loop/LoopFor.java
@@ -2,10 +2,8 @@ package ru.job4j.loop;
 
 public class LoopFor {
     public static void main(String[] args) {
-        int i;
-        for (i = 5; i <= 10; i++) {
+        for (int i = 5; i <= 10; i++) {
             System.out.println(i);
         }
-        System.out.println("Индекс после цикла равен: " + i);
     }
 }
