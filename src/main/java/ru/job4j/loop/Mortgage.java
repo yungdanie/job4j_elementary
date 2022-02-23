5b5788f54e697ce6dcb52848dd54f713a77e25b9
yungdanie <nikoches253@gmail.com>
2022-02-23 14:03:35 +0300

    5.6. Простые числа [#156314 #268494]

---
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,12 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public static int year(double amount, double salary, double percent) {
+        int years = 0;
+        while (amount >= 0) {
+            amount = amount + (amount * percent / 100) - salary;
+            years++;
+        }
+        return years;
+    }
+}
